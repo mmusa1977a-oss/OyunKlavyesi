22d47d197dfa4b6b297b7c05d2c3aefb6e475972
mmusa1977a-oss <mmusa1977a@gmail.com>
2026-05-17 14:53:10 +0300

    Update MainActivity.java

--- app/src/main/java/com/gursoy/oyunklavyesi/MainActivity.java
+++ app/src/main/java/com/gursoy/oyunklavyesi/MainActivity.java
@@ -1,62 +1,36 @@
-package com.gursoy.oyunklavyesi;
+package com.araziarabasi.yildizavi;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.app.Activity;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.widget.Toast;
+import android.view.Window;
+import android.view.WindowManager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
-
-    WebView webView;
-    private static final int OVERLAY_REQ_CODE = 1001;
+public class MainActivity extends Activity {
+    private GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        webView = findViewById(R.id.webView);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
 
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setMediaPlaybackRequiresUserGesture(false);
-
-        webView.loadUrl("https://www.gameszap.com/game/2141/dune-buggy.html");
-
-        checkOverlayPermission();
+        gameView = new GameView(this);
+        setContentView(gameView);
     }
 
-    private void checkOverlayPermission() {
-        if (!Settings.canDrawOverlays(this)) {
-            Toast.makeText(this, "Diğer uygulamaların üstünde göster iznini aç", Toast.LENGTH_LONG).show();
-
-            Intent intent = new Intent(
-                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:" + getPackageName())
-            );
-            startActivityForResult(intent, OVERLAY_REQ_CODE);
-        } else {
-            startFloatingService();
-        }
-    }
-
-    private void startFloatingService() {
-        Intent intent = new Intent(this, FloatingControlService.class);
-        startService(intent);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pause();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        if (Settings.canDrawOverlays(this)) {
-            startFloatingService();
-        }
+        gameView.resume();
     }
 }
